c9d396ec50cbfa65d473f6092a9aee4f3a0c2b5b
DBaldwin8 <darren_baldwin@live.com>
2024-10-16 15:16:40 +0100

    text size sent, container of text flex col

--- src/WebAppContainer/index.jsx
+++ src/WebAppContainer/index.jsx
@@ -1,9 +1,9 @@
 function WebAppContainer() {
     return (
     <div>
-            <div>
-                <p className='tracking-widest'>SPLI</p>
-                <p>TTER</p>
+            <div className='flex flex-col'>
+                <p className='text-3xl'>SPLI</p>
+                <p className='text-3xl'>TTER </p>
             </div>
 
     </div>
