bc4c06d243ef45329486b35266b8870fffdb17d1
DBaldwin8 <darren_baldwin@live.com>
2024-10-16 16:07:00 +0100

    flex and col input boxes below their labels.

--- src/WebAppContainer/index.jsx
+++ src/WebAppContainer/index.jsx
@@ -4,13 +4,13 @@
 function WebAppContainer() {
     return (
     <div>
-            <div className='flex flex-col items-center mt-28'>
+            <div className='flex flex-col items-center mt-28 mb-12'>
                 <p className='text-3xl'>SPLI</p>
                 <p className='text-3xl'>TTER </p>
             </div>
             <div className="bg-white w-3/4 h-1/2 mx-auto rounded-3xl flex place-content-around">
-                <form className='border border-black'>
-                    <div>
+                <form className=''>
+                    <div className='flex flex-col'>
                         <label htmlFor=''>Bill</label>
                         <input id='' type='number' placeholder='0' className="placeholder:text-right"/>
                     </div>
@@ -18,12 +18,12 @@ function WebAppContainer() {
                         <label htmlFor=''>Select Tip %</label>
                         
                     </div>
-                    <div>
+                    <div className='flex flex-col'>
                         <label htmlFor=''>Number of People</label>
                         <input id='' type='number' placeholder='0' className="placeholder:text-right"/>
                     </div>
                 </form>
-                <div className='border border-black'>
+                <div className=''>
                     <div>
                         <h3>Tip Amount</h3>
                         <p>/ person</p>
