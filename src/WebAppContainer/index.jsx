f9022dee4e4547a430e9a11cb45b2274753fbc69
DBaldwin8 <darren_baldwin@live.com>
2024-10-16 16:01:05 +0100

    content around and border for vis added to center container

--- src/WebAppContainer/index.jsx
+++ src/WebAppContainer/index.jsx
@@ -8,8 +8,8 @@ function WebAppContainer() {
                 <p className='text-3xl'>SPLI</p>
                 <p className='text-3xl'>TTER </p>
             </div>
-            <div className="bg-white w-3/4 h-1/2 mx-auto rounded-3xl">
-                <form>
+            <div className="bg-white w-3/4 h-1/2 mx-auto rounded-3xl flex place-content-around">
+                <form className='border border-black'>
                     <div>
                         <label htmlFor=''>Bill</label>
                         <input id='' type='number' placeholder='0' className="placeholder:text-right"/>
@@ -23,7 +23,7 @@ function WebAppContainer() {
                         <input id='' type='number' placeholder='0' className="placeholder:text-right"/>
                     </div>
                 </form>
-                <div>
+                <div className='border border-black'>
                     <div>
                         <h3>Tip Amount</h3>
                         <p>/ person</p>
