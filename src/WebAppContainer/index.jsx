7ca6f5a9d13bb40abc3df02de672d718f536379e
DBaldwin8 <darren_baldwin@live.com>
2024-10-16 15:23:06 +0100

    text moved from top using margin, started structure for center form, moving to component for input boxes

--- src/WebAppContainer/index.jsx
+++ src/WebAppContainer/index.jsx
@@ -1,11 +1,19 @@
 function WebAppContainer() {
     return (
     <div>
-            <div className='flex flex-col'>
+            <div className='flex flex-col items-center mt-28'>
                 <p className='text-3xl'>SPLI</p>
                 <p className='text-3xl'>TTER </p>
             </div>
+            <div>
+                <form>
+                    <p>Bill</p>
+                    
+                </form>
+                <div>
 
+                </div>
+            </div>
     </div>
     )
 }
