7582e68145bba6751fe18cfccd76b87ad261794c
DBaldwin8 <darren_baldwin@live.com>
2024-10-16 15:01:02 +0100

    initital file dump, created WebAppContainer

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import WebAppContainer from './WebAppContainer/index.jsx'
+
+function App() {
+
+  return (
+    <>
+      <WebAppContainer></WebAppContainer>
+    </>
+  )
+}
+
+export default App
---
+++ src/WebAppContainer/index.jsx
@@ -0,0 +1,9 @@
+function WebAppContainer() {
+    return (
+    <div className="bg-cyan-100 h-full w-full">
+
+    </div>
+    )
+}
+
+export default WebAppContainer
