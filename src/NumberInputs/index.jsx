0bde8cda1b9e93abf1bbe7dd6d9681deb841c9a9
DBaldwin8 <darren_baldwin@live.com>
2024-10-17 19:49:35 +0100

    number inputs height set 8

--- src/NumberInputs/index.jsx
+++ src/NumberInputs/index.jsx
@@ -4,7 +4,7 @@ function NumberInputs({id, name, step, handleChange}) {
         <>
             <div className='flex flex-col'>
                 <label htmlFor={id} className="text-xs pb-2">{name}</label>
-                <input id={id} type='number' placeholder='0' step={step} className="text-right bg-slate-50 w-full rounded-md" onChange={handleChange}/>
+                <input id={id} type='number' placeholder='0' step={step} className="text-right bg-slate-50 h-8 w-full rounded-md" onChange={handleChange}/>
             </div>
         </>
     )
