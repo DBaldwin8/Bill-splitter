e497f272360707c2faa5751986f44354fb7ccb70
Darren <darren_baldwin@live.com>
2025-01-31 20:41:24 +0000

    rouge space

--- src/NumberInputs/index.jsx
+++ src/NumberInputs/index.jsx
@@ -3,7 +3,7 @@ function NumberInputs({id, name, step, min, value, handleChange}) {
     return (
         <div className='flex flex-col'>
             <label htmlFor={id} className="text-xs pb-2 text-slate-500">{name}</label>
-            <input id={id} type='number' step={step} min={min} value={value} onChange={handleChange} className={`text-right bg-slate-50 h-8 pt-2 rounded-md w-full `}/>
+            <input id={id} type='number' step={step} min={min} value={value} onChange={handleChange} className={`text-right bg-slate-50 h-8 pt-2 rounded-md w-full`}/>
         </div>
     )
 }
