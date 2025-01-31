15d031aca93b306b57dc039eba077f5bbe76fb10
Darren <darren_baldwin@live.com>
2025-01-31 09:16:32 +0000

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 import './App.css'
 import WebAppContainer from './WebAppContainer/index.jsx'
 
--- src/Button/index.jsx
+++ src/Button/index.jsx
@@ -1,14 +1,4 @@
 function Button({tipPerc, clickHandler}){
-    /*
-    let count = 0;
-    const handleClick = (event) => {
-        count++;
-        console.log(count);
-    }
-
-    return <button onClick={handleClick} className="bg-teal-900 rounded-md text-white h-14 w-56 text-center">Clicked {count} times!</button>
-    */
-
    return <button className="bg-teal-900 rounded-md text-white h-8 w-24 text-center" id='tip' onClick={clickHandler}>{tipPerc}%</button>
 }
 
